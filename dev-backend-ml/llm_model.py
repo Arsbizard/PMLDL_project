93c095fa1d128f206cdaa538794147617f012f3f
Sinyavka <alsiniagina@yandex.ru>
2024-12-09 05:34:47 +0300

    docker fix

--- dev-backend-ml/llm_model.py
+++ dev-backend-ml/llm_model.py
@@ -3,7 +3,7 @@ import json
 
 class LLM:
     def __init__(self):
-        self.url = "http://0.0.0.0:8001/generate"
+        self.url = "http://192.168.3.5:8011/generate"
 
     def generate_response(self, query, docs_rel):
         prompt = (
